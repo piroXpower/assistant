15f57c8cd0c66b08b29e7da69b2de9295cf8490e
丂ﾑﾶﾶƳ <89745675+ProXSammY@users.noreply.github.com>
2022-01-21 10:47:45 +0530

    Update __init__.py

--- assistant/__init__.py
+++ assistant/__init__.py
@@ -1,1 +1,12 @@
+# Powered By: © TeamRaichu
+# Copyright (C) 2022 @OfficialRaichu
 
+from telethon import TelegramClient
+from assistant.Conf import Config
+import logging
+ 
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.INFO)
+
+bot = TelegramClient('Sammy', api_id=Config.APP_ID, api_hash=Config.API_HASH)
+Sammy = bot.start(bot_token=Config.TOKEN)
