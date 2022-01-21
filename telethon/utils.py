935c7676546416465cd2f0261391b9c67fe521a1
丂ﾑﾶﾶƳ <89745675+ProXSammY@users.noreply.github.com>
2022-01-21 10:28:42 +0530

    Create utils.py

---
+++ telethon/utils.py
@@ -0,0 +1,17 @@
+# Powered By: © TeamRaichu
+# Copyright (C) 2022 @OfficialRaichu
+
+import sys
+import logging
+import importlib
+from pathlib import Path
+
+def load_plugins(plugin_name):
+    path = Path(f"telethon/plugins/{plugin_name}.py")
+    name = "telethon.plugins.{}".format(plugin_name)
+    spec = importlib.util.spec_from_file_location(name, path)
+    load = importlib.util.module_from_spec(spec)
+    load.logger = logging.getLogger(plugin_name)
+    spec.loader.exec_module(load)
+    sys.modules["telethon.plugins." + plugin_name] = load
+    print("Bot has Started Successfully " + plugin_name)
