96fdd8b92cadc2864c9cec3b5cd7c716d404c568
丂ﾑﾶﾶƳ <89745675+ProXSammY@users.noreply.github.com>
2022-01-21 10:50:01 +0530

    Create __main__.py

---
+++ assistant/__main__.py
@@ -0,0 +1,24 @@
+# Powered By: © TeamRaichu
+# Copyright (C) 2022 @OfficialRaichu
+
+import glob
+from pathlib import Path
+from assistant.utils import load_plugins
+import logging
+from assistant import Sammy
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.INFO)
+
+path = "assistant/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins(plugin_name.replace(".py", ""))
+    
+print("Successfully Started Your Bot!")
+
+if __name__ == "__main__":
+    Sammy.run_until_disconnected()
