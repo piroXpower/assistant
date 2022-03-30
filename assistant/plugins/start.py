c9bec17d87800f300e859ed321866997e7954387
丂ﾑﾶﾶƳ <89745675+FriDayXD@users.noreply.github.com>
2022-03-30 15:40:10 +0530

    Update start.py

--- assistant/plugins/start.py
+++ assistant/plugins/start.py
@@ -3,7 +3,7 @@ from telethon import events, Button
 
 PM_START_TEXT = """
 **Hi {}**
-I am a bot who works for @RaiChuOfficial and can detect spammers in groups can protect groups from then
+I am a bot who works for @TeamDeCoDe and can detect spammers in groups can protect groups from then
 
 **Click the below button for getting help menu!**
 """
@@ -14,7 +14,7 @@ async def start(event):
     if event.is_private:
        await event.reply(PM_START_TEXT.format(event.sender.first_name), buttons=[
         [Button.inline("Help & Commands", data="help")],
-        [Button.url("CreDits", "https://t.me/OfficialShield")]])
+        [Button.url("CreDits", "https://t.me/DeCoDeDevs")]])
        return
 
     if event.is_group:
