d8568d70e426bd874959ae0b52928ec1518e5551
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-04-03 16:05:33 +0530

    Update help.py

--- assistant/plugins/help.py
+++ assistant/plugins/help.py
@@ -3,11 +3,21 @@ from assistant import Sammy
 from assistant.Conf import Config
 
 btn =[
-    [Button.inline("Admin", data="admin"), Button.inline("Bans", data="bans")],
-    [Button.inline("Pins", data="pins"), Button.inline("Pugres", data="purges")],
-    [Button.inline("Locks", data="locks"), Button.inline("Misc", data="misc")],
-    [Button.inline("Chat Cleaner", data="zombies")],
-    [Button.inline("Back", data="start")]]
+      [
+        Button.inline("Admin", data="admin"),
+        Button.inline("Bans", data="bans"), 
+        Button.inline("Pins", data="pins")
+      ], 
+      [ 
+        Button.inline("Pugres", data="purges"), 
+        Button.inline("Locks", data="locks"),
+        Button.inline("Misc", data="misc")
+      ],
+      [
+        Button.inline("Chat Cleaner", data="zombies")],
+        Button.inline("Back", data="start")
+      ]
+     ]
 
 HELP_TEXT = """
 **Heya {} help menu here:**
@@ -15,7 +25,7 @@ HELP_TEXT = """
 /start - To Start Me ;)
 /help - To Get Available Help Menu
 
-__Report Bugs At--->__ **@DeCodeSupport**
+__Report Bugs At--->__ **@TheeDeCoDe**
 All cmd can be used with ! or ? or /.
 """.format(Config.BOT_USERNAME)
 
@@ -24,8 +34,13 @@ All cmd can be used with ! or ? or /.
 async def help(event):
 
     if event.is_group:
-       await event.reply("Contact me in PM to get available help menu!", buttons=[
-       [Button.url("Help And Commands!", "T.me/{}?start=help".format(Config.BOT_USERNAME))]])
+       await event.reply("Contact me in PM to get available help menu!",
+ buttons=[
+          [
+            Button.url("Help And Commands!", "T.me/{}?start=help".format(Config.BOT_USERNAME))
+          ]
+         ]
+)
        return
 
     await event.reply(HELP_TEXT, buttons=btn)
@@ -43,6 +58,11 @@ async def _(event):
 @Sammy.on(events.callbackquery.CallbackQuery(data="start"))
 async def _(event):
 
-     await event.edit(HELP_TEXT, buttons=[
-        [Button.inline("Help & Commands", data="help")],
-        [Button.url("CreDits", "https://t.me/DeCoDeDevs")]])
+     await event.edit(HELP_TEXT, 
+buttons=[
+         [
+           Button.inline("Help & Commands", data="help"), 
+           Button.url("CreDits", "https://t.me/DeCoDeDevs")
+         ]
+        ]
+)
