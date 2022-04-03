a54c53418637fc419c8cf4ffc2ce3b6eccab3700
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-04-03 16:54:37 +0530

    Create utils.py

---
+++ assistant/utils.py
@@ -0,0 +1,51 @@
+import glob
+import logging
+import sys
+from pathlib import Path
+
+from telethon import events
+
+from assistant import Sammy as tbot
+
+
+def Cbot(**args):
+    pattern = args.get("pattern", None)
+    r_pattern = r"^[/?!+]"
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+    args["pattern"] = pattern.replace("^/", r_pattern, 1)
+
+    def decorator(func):
+        async def wrapper(check):            
+            try:
+                await func(check)
+            except BaseException:
+                return
+            else:
+                pass
+
+        tbot.add_event_handler(wrapper, events.NewMessage(**args))
+        return wrapper
+
+    return decorator
+
+
+def Cquery(**args):
+    pattern = args.get("pattern", None)
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    def decorator(func):
+        tbot.add_event_handler(func, events.InlineQuery(**args))
+        return func
+
+    return decorator
+
+
+def Cinline(**args):
+    def decorator(func):
+        tbot.add_event_handler(func, events.CallbackQuery(**args))
+        return func
+
+    return decorator
