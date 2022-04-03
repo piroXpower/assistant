79bf51fd43101e3b459006e6bbb4081403f926cc
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-04-03 16:19:52 +0530

    Update start.py

--- assistant/plugins/start.py
+++ assistant/plugins/start.py
@@ -3,7 +3,7 @@ from telethon import events, Button
 
 PM_START_TEXT = """
 **Hi {}**
-I am a bot who works for @TeamDeCoDe and can detect spammers in groups can protect groups from then
+I am a bot who works for **@TeamDeCoDe** and can detect spammers in groups can protect groups from then
 
 **Click the below button for getting help menu!**
 """
